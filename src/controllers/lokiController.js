2250b7a876a4647135685b28d7d4da00320f5e17
Aleksandar Tendjer <tendjer@protonmail.com>
2022-05-27 12:56:59 +0200

    added logic when query is empty to return the pair for EN

--- src/controllers/lokiController.js
+++ src/controllers/lokiController.js
@@ -1,6 +1,12 @@
 const lokiModel = require("../models/lokiModel");
 exports.getPairByLanguage = async (req, res) => {
-  lokiModel.retLangWordPairs(req.query?.lang).then(function (pair) {
-    return res.json(200, pair.word);
-  });
+  if (Object.keys(req?.query).length === 0) {
+    lokiModel.retLangWordPairs("EN").then(function (pair) {
+      return res.json(200, pair.word);
+    });
+  } else {
+    lokiModel.retLangWordPairs(req.query?.lang).then(function (pair) {
+      return res.json(200, pair.word);
+    });
+  }
 };
